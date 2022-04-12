98038425b62cb6dd7e4715b4d08c851f3f1956ab
NPQ-1807 <89781959+NPQ-1807@users.noreply.github.com>
2022-04-12 18:59:01 +0700

    first commit

---
+++ models/User.js
@@ -0,0 +1,31 @@
+const mogoose = require("mongoose");
+
+const userSchema = new mogoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        minlength: 6,
+        maxlength: 20,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        minlength: 10,
+        maxlength: 50,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    admin: {
+        type: Boolean,
+        default: false,
+    }
+},
+    { timestamps: true }
+)
+
+module.exports = mogoose.model("User", userSchema);
